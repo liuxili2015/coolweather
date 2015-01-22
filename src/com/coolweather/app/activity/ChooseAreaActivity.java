bd95ba08b8505a55bb2f73b664779561733c4683
liuxl <liuxl@gmail.com>
2015-01-23 01:26:43 +0800

    加入显示天气信息的功能。

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -20,7 +20,10 @@ import com.coolweather.app.util.Utility;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -79,6 +82,13 @@ public class ChooseAreaActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		if(prefs.getBoolean("city_selected", false)){
+			Intent intent = new Intent(this,WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		listView = (ListView) findViewById(R.id.list_view);
@@ -98,6 +108,12 @@ public class ChooseAreaActivity extends Activity {
 				}else if(currentLevel == LEVEL_CITY){
 					selectedCity = cityList.get(position);
 					queryCounties();
+				}else if(currentLevel == LEVEL_COUNTY){
+					String countyCode = countyList.get(position).getCountyCode();
+					Intent intent = new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+					intent.putExtra("count_code", countyCode);
+					startActivity(intent);
+					finish();
 				}
 			}
 		});
---
+++ src/com/coolweather/app/activity/WeatherActivity.java
@@ -0,0 +1,183 @@
+package com.coolweather.app.activity;
+
+
+import com.coolweather.app.R;
+import com.coolweather.app.util.HttpCallbackListener;
+import com.coolweather.app.util.HttpUtil;
+import com.coolweather.app.util.Utility;
+
+import android.app.Activity;
+import android.app.SearchManager.OnCancelListener;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class WeatherActivity extends Activity implements OnClickListener{
+
+	private LinearLayout weatherInfoLayout;
+	
+	/**
+	 * 用于显示城市名
+	 */
+	private TextView cityNameText;
+	/**
+	 * 用于显示发布时间
+	 */
+	private TextView publishText;
+	/**
+	 * 用于显示天气描述
+	 */
+	private TextView weatherDespText;
+	/**
+	 * 用于显示气温1
+	 */
+	private TextView temp1Text;
+	/**
+	 * 用于显示气温2
+	 */
+	private TextView temp2Text;
+	/**
+	 * 用于显示当前日期
+	 */
+	private TextView currentDateText;
+	/**
+	 * 切换城市按钮
+	 */
+	private Button switchCity;
+	/**
+	 * 更新天气按钮
+	 */
+	private Button refreshWeather;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.weather_layout);
+		//初始化各个控件
+		weatherInfoLayout = (LinearLayout) findViewById(R.id.weather_info_layout);
+		cityNameText = (TextView) findViewById(R.id.city_name);
+		publishText = (TextView) findViewById(R.id.publish_text);
+		weatherDespText = (TextView) findViewById(R.id.weather_desp);
+		temp1Text = (TextView) findViewById(R.id.temp1);
+		temp2Text = (TextView) findViewById(R.id.temp2);
+		currentDateText = (TextView) findViewById(R.id.current_date);
+		/*switchCity = findViewById(R.id.switch_city);
+		refreshWeather = findViewById(R.id.refreshWeather);*/
+		String countyCode = getIntent().getStringExtra("county_code");
+		if(!TextUtils.isEmpty(countyCode)){
+			//县级代号时候就去查询天气
+			publishText.setText("同步中...");
+			weatherInfoLayout.setVisibility(View.INVISIBLE);
+			cityNameText.setVisibility(View.INVISIBLE);
+			queryWeatherCode(countyCode);
+		}else{
+			showWeather();
+		}
+		/*switchCity.setOnClickListener(this);
+		refreshWeather.setOnClickListener(this);*/
+	}
+
+
+
+
+
+
+
+	private void showWeather() {
+		// TODO Auto-generated method stub
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		cityNameText.setText(prefs.getString("city_name", ""));
+		temp1Text.setText(prefs.getString("temp1", ""));
+		temp2Text.setText(prefs.getString("temp2", ""));
+		weatherDespText.setText(prefs.getString("weather_desp", ""));
+		publishText.setText("今天"+prefs.getString("publish_time", "")+"发布");
+		currentDateText.setText(prefs.getString("current_date", ""));
+		weatherInfoLayout.setVisibility(View.VISIBLE);
+		cityNameText.setVisibility(View.VISIBLE);
+	}
+
+
+
+
+
+
+
+	private void queryWeatherCode(String countyCode) {
+		// TODO Auto-generated method stub
+		String address = "http://www.weather.com.cn/data/list3/city"+countyCode+".xml";
+		queryFromServer(address,"countyCode");
+	}
+
+
+	private void queryWeatherInfo(String weatherCode) {
+		// TODO Auto-generated method stub
+		String address = "http://www.weather.com.cn/data/list3/city"+weatherCode+".xml";
+		queryFromServer(address,"weatherCode");
+	}
+
+
+
+	private void queryFromServer(final String address, final String type) {
+		// TODO Auto-generated method stub
+		HttpUtil.sendRequest(address, new HttpCallbackListener() {
+			
+			@Override
+			public void onFinish(String response) {
+				// TODO Auto-generated method stub
+				if("countyCode".equals(type)){
+					if(!TextUtils.isEmpty(response)){
+						//从服务器返回的数据中解析出天气代号
+						String[] array = response.split("\\|");
+						if(array != null && array.length>0){
+							String weatherCode = array[1];
+							queryWeatherInfo(weatherCode);
+						}
+					}
+				}else if("weatherCode".equals(type)){
+					//处理服务器返回的天气数据
+					Utility.handleWeatherResponse(WeatherActivity.this, response);
+					runOnUiThread(new Runnable() {
+						public void run() {
+							showWeather();
+						}
+					});
+				}
+			}
+			
+
+			@Override
+			public void onError(Exception e) {
+				// TODO Auto-generated method stub
+				runOnUiThread(new Runnable() {
+					public void run() {
+						publishText.setText("同步失败");
+					}
+				});
+			}
+		});
+	}
+
+	
+
+
+
+
+
+	@Override
+	public void onClick(DialogInterface dialog, int which) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
